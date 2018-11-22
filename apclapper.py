69e73ad53b35d79a36039a19ec040d2d8e192ecc
jobro247 <44535515+jobro247@users.noreply.github.com>
2018-11-21 20:54:28 -0800

    Update apclapper.py

--- apclapper.py
+++ apclapper.py
@@ -1,4 +1,5 @@
 import os
+os.system("git pull")
 print ("if you want to exit the program just press CTRL and c to stop")
 print ("when running airodump-ng just press ctrl c to stop")
 os.system("airmon-ng start wlan0")
