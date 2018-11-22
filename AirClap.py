bf819e13dee4e5322d80652c11bdbf35a39164d7
Josiah Crofton <44535515+jobro247@users.noreply.github.com>
2018-11-22 15:37:58 -0800

    Update AirClap.py

--- AirClap.py
+++ AirClap.py
@@ -39,6 +39,5 @@ if main_choice == 'e':
   os.system("airodump-ng --channel %s --bssid %s wlan0mon" % (e, r))
   menu_loop = raw_input("Would you like to return to main menu? y/n:")
   if menu_loop == 'y':
-    loop
-  else:
+    
     break
